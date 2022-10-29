39d03bd1432e50ea6bfa0e856d7f11500196d79e
affali <affalistephane@gmail.com>
2022-10-29 08:22:11 +0000

    terminé reste à améliorer

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,28 +5,31 @@ import { getUser } from '../redux/actions/actions';
 
 const SearchBar = () => {
 
-    const [searchUser, setSearchUser] = useState('stephaff');
+    const [searchUser, setSearchUser] = useState('');
     const dispatch = useDispatch();
 
-    const fetchUser = (searchUser) =>{
-        fetch(`https://api.github.com/users/${ searchUser }`)
+    let InitialUser = 'stephaff';
+
+    const fetchUser = (user) =>{
+        fetch(`https://api.github.com/users/${ user }`)
         .then(res => res.json())
         .then(json => dispatch(getUser(json)))
     }
 
     useEffect(() => {
-        fetchUser(searchUser);
+        fetchUser(InitialUser);
     }, []);
 
     const displayUser = () => {
-        fetchUser(searchUser);
+        let user = searchUser;
+        fetchUser(user);
     }
 
     return (
         <div className='searchBar'>
             <div className="searchBar-container">
                 <input type="search" placeholder='Search github user' onChange={ e => setSearchUser(e.target.value) } />
-                <button className='btn-search' onClick={ displayUser }>Search</button>
+                <button className='btn-search' onClick={ displayUser } >Search</button>
             </div>
         </div>
     );
